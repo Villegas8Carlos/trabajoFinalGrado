6488c00314198dddefa5bb08053bad175276232b
Villegas8Carlos <carlosvillegascarrizo@gmail.com>
2025-05-25 12:55:25 +0200

    Commit

---
+++ src/pages/RegisterPage.jsx
@@ -0,0 +1,40 @@
+import FormularioUsuario from "../components/RegisterForm";
+
+<<<<<<< HEAD
+const FormPage = () => {
+  return (
+    <div>
+      <header className="menu">
+        <nav>
+          <ul>
+            <img src="/images/" alt="logoNombre" />
+          </ul>
+        </nav>
+      </header>
+      <FormularioUsuario />
+      <footer>
+        <a href="" target="_blank" rel="noopener noreferrer">
+          <img src="/images/" alt="twitter" />
+        </a>
+        <img src="/images/" alt="Logo" />
+        <a href="" target="_blank" rel="noopener noreferrer">
+          <img src="/images/" alt="linkedin" />
+        </a>
+        <p>Iniciativa de </p>
+        <p>© Copyright 2025 Empresa</p>
+      </footer>
+=======
+const RegisterPage = () => {
+  return (
+    <div>
+      <FormularioUsuario />
+>>>>>>> 55ab3b0 (Commit)
+    </div>
+  );
+};
+
+<<<<<<< HEAD
+export default FormPage;
+=======
+export default RegisterPage;
+>>>>>>> 55ab3b0 (Commit)
